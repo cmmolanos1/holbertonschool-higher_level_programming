8c9433accbf862da76a1ed488d4c94c1af6846cf
Carlos Molano <926@holbertonschool.com>
2019-09-10 12:34:20 -0500

    Fixing 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,7 +2,7 @@
 for i in range(10):
     for j in range(10):
         if j > i and i != 8:
-            print("{}{}".format(i, j), end = ", ")
+            print("{}{}".format(i, j), end=", ")
         elif i == 8 and j == 9:
             print("{}{}".format(i, j))
             break
