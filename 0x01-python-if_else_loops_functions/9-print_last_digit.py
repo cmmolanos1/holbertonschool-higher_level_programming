caa3bd70b00893897fdb79a79b14e27008a1d55c
Carlos Molano <926@holbertonschool.com>
2019-09-10 13:03:23 -0500

    Fixing 9

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -2,7 +2,5 @@
 def print_last_digit(number):
     if number < 0:
         number = -number
-    else:
-        number = number
-    print(number % 10, end = '')
+    print(number % 10, end='')
     return(number % 10)
