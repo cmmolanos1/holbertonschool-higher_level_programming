434e92c82f23518dfd7cff19ec6fcc3bd6bae217
Carlos Molano <926@holbertonschool.com>
2019-09-12 12:13:31 -0500

    Fixing 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-import add_0
+if __name__ == "__main__":
+    
+    import add_0
 
-a = 1
-b = 2
+    a = 1
+    b = 2
 
-print("{} + {} = {}".format(a, b, add_0.add(a, b)))
+    print("{:d} + {:d} = {:d}".format(a, b, add_0.add(a, b)))
