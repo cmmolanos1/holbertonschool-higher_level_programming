567e8bd8644abc72e26bf510828613f36a460b80
Carlos Molano <926@holbertonschool.com>
2019-09-30 14:40:20 -0500

    Fixing 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    counter = 0
     for i in range(x):
         try:
             print(my_list[i], end='')
-            i += 1
+            counter += 1
         except:
             break
     print('')
-    return i
+    return counter
