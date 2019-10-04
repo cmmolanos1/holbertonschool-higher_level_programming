777d474eee44e89a603b8be278deb32fcbf0df07
Carlos Molano <926@holbertonschool.com>
2019-10-04 16:39:27 -0500

    Uploading some fix of 5

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def text_indentation(text):
-    if type(text) is not str:
+    if type(text) is not str or text is None or len(text) < 0:
         raise TypeError('text must be a string')
 
     t = list(text)
@@ -9,9 +9,11 @@ def text_indentation(text):
         if t[i] == '.' or t[i] == '?' or t[i] == ':':
             if i + 1 != len(t):
                 if t[i + 1] == '.' or t[i + 1] == '?' or t[i + 1] == ':':
-                    t.insert(i + 1, '\n')
+                    t.insert(i + 1, '\n\n')
                 else:
                     t[i + 1] = '\n\n'
+            else:
+                t.insert(i + 1, '\n\n')
 
     text2 = "".join(t)
     print(text2, end='')
