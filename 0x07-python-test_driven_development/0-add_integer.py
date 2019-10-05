b6b22a36146c0dc879641dec8c17193d808dc5dc
Carlos Molano <926@holbertonschool.com>
2019-10-05 08:08:44 -0500

    Fix 0: Changing for isinstance

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,8 +1,19 @@
 #!/usr/bin/python3
+"""
+This module has a function that adds two numbers
+"""
+
+
 def add_integer(a, b=98):
+    """
+    Args:
+        a: first number
+        b: second number
+    Returns:
+        The addition of the two given numbers
+    """
     if not isinstance(a, (int, float)):
         raise TypeError('a must be an integer')
     if not isinstance(b, (int, float)):
         raise TypeError('b must be an integer')
-
     return int(a) + int(b)
