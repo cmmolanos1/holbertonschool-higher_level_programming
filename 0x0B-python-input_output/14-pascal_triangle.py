6dd1911f872b6ac782ceeec09a9fda06edc39a89
Carlos Molano <926@holbertonschool.com>
2019-10-16 07:20:16 -0500

    [Upload] 14

---
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def pascal_triangle(n):
+    """Prints the n-pascal triangle"""
+    lista = []
+    if n <= 0:
+        lista = [[]]
+        return lista
+    for i in range(0, n):
+        lista.append([1] * (i + 1))
+        if i >= 2:
+            for j in range(1, len(lista[i]) - 1):
+                lista[i][j] = lista[i - 1][j] + lista[i - 1][j - 1]
+    return lista
