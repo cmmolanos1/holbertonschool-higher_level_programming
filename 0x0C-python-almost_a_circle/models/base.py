beafb376954c8878c183fd4241efb64638f26fbd
Carlos Molano <926@holbertonschool.com>
2019-10-18 11:36:21 -0500

    [Upload] 1

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+class Base():
+    """Base class of all other classes in this project
+    The goal of it is to manage id attribute in all classes
+    and to avoid duplicating the same code (by extension, same bugs)"""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Initialize the class"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = self.__nb_objects
