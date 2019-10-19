421210a8b1531710b970881f41d2ef5e3d598ed9
Carlos Molano <926@holbertonschool.com>
2019-10-19 11:30:59 -0500

    [Update] 12

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Class Square"""
+    def __init__(self, size, x=0, y=0, id=None):
+        """Initialize the class"""
+        super().__init__(size, size, x, y, id)
+        self.size = size
+
+    def __str__(self):
+        """Print information about the Rectangle"""
+        return "[Square] (" + str(self.id) + ") " + str(self.x) \
+            + "/" + str(self.y) + " - " + str(self.size)
+
+    @property
+    def size(self):
+        """size getters"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """setter for size"""
+        self.width = value
+        self.height = value
+
+    def update(self, *args, **kwargs):
+        """Updating the class reading variable arguments"""
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.size = args[1]
+            if len(args) >= 4:
+                self.x = args[2]
+            if len(args) == 5:
+                self.y = args[3]
+        else:
+            if "id" in kwargs:
+                self.id = kwargs["id"]
+            if "size" in kwargs:
+                self.size = kwargs["size"]
+            if "x" in kwargs:
+                self.x = kwargs["x"]
+            if "y" in kwargs:
+                self.y = kwargs["y"]
