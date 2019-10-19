dfcddd6f9ce335d0c6c3724380425de1952f2509
Carlos Molano <926@holbertonschool.com>
2019-10-19 11:39:34 -0500

    [Update] 14

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -112,3 +112,14 @@ class Rectangle(Base):
                 self.x = kwargs["x"]
             if "y" in kwargs:
                 self.y = kwargs["y"]
+
+    def to_dictionary(self):
+        """returns the dictionary representation of a Rectangle"""
+        dictionary = {}
+        dictionary['id'] = self.id
+        dictionary['width'] = self.width
+        dictionary['height'] = self.height
+        dictionary['x'] = self.x
+        dictionary['y'] = self.y
+
+        return dictionary
--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -47,3 +47,13 @@ class Square(Rectangle):
                 self.x = kwargs["x"]
             if "y" in kwargs:
                 self.y = kwargs["y"]
+
+    def to_dictionary(self):
+        """returns the dictionary representation of a Square"""
+        dictionary = {}
+        dictionary['id'] = self.id
+        dictionary['size'] = self.size
+        dictionary['x'] = self.x
+        dictionary['y'] = self.y
+
+        return dictionary
