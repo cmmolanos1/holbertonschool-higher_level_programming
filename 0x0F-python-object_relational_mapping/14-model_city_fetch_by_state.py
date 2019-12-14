1c47eff16c56de98acaa6e1185d5cbe8ac3f1311
Carlos Molano <926@holbertonschool.com>
2019-12-14 18:39:32 -0500

    Adding brackets

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -18,8 +18,8 @@ if __name__ == "__main__":
     session = Session()
 
     for city, state in session.query(City, State).\
-             filter(City.state_id == State.id).\
-             order_by(City.id):
-        print("{}: {} {}".format(state.name, city.id, city.name))
+            filter(City.state_id == State.id).\
+            order_by(City.id):
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
 
     session.close()
