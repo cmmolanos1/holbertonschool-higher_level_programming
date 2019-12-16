1d8d9859c13bff9411204ebc69a80956eb4cb14a
Carlos Molano <926@holbertonschool.com>
2019-12-16 09:57:39 -0500

    Changing the imporation to relationship

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -6,7 +6,7 @@
 
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-from model_state import Base, State
+from relationship_state import State, Base
 
 
 class City(Base):
