f34b64ecab3aec624b63ad60a531fbdcaa3a77c4
Carlos Molano <926@holbertonschool.com>
2020-01-06 12:42:26 -0500

    Fixing requirements

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# sends a GET request to the URL, and displays the body of the response.
+# Sends a GET request to the URL, and displays the body of the response.
 curl -sI "$1" | head -n 1 | cut -d' ' -f 2
