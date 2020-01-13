ddcda0901fa46482dde94407597f7bb621982183
Carlos Molano <926@holbertonschool.com>
2020-01-13 14:29:24 -0500

    Re-making the loop

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -11,10 +11,9 @@ if __name__ == "__main__":
     repo = sys.argv[1]
     owner = sys.argv[2]
     url = 'https://api.github.com/repos/{}/{}/commits'.format(owner, repo)
-    response = requests.get(url).json()
+    commits = requests.get(url).json()
 
-    commits = response[:10]
-    for commit in commits:
-        sha = commit['sha']
-        author = commit['commit']['author']['name']
+    for i in range(10):
+        sha = commits[i]['sha']
+        author = commits[i]['commit']['author']['name']
         print("{}: {}".format(sha, author))
