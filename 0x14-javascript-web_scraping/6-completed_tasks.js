795e58d49e6f5afabf9923a3c12661eae00dd8ce
Carlos Molano <926@holbertonschool.com>
2020-02-11 13:14:43 -0500

    Fix 6

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
 const url = process.argv[2];
-
 request(url, function (error, response, body) {
   if (error) {
     console.log(error);
@@ -17,7 +16,6 @@ request(url, function (error, response, body) {
         }
       }
     }
-
     console.log(JSON.stringify(completed));
   }
 });
