b19667d14f25f2351ab7077534241ec11781e281
Carlos Molano <926@holbertonschool.com>
2020-02-11 12:47:50 -0500

    Fix try

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.readFileSync(process.argv[2], 'utf8', (err, data) => {
+fs.readFile(process.argv[2], 'utf-8', (err, data) => {
   if (err) {
     console.log(err);
   } else {
