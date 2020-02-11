cb944cf783f215fdbc760d793e754484849364ef
Carlos Molano <926@holbertonschool.com>
2020-02-11 13:01:54 -0500

    Fix 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
 const url = process.argv[2];
-
 request(url, function (error, response, body) {
   if (error) {
     console.log(error);
@@ -10,7 +9,7 @@ request(url, function (error, response, body) {
     const films = JSON.parse(body).results;
     for (const film of films) {
       for (const charac of film.characters) {
-        if (charac === 'https://swapi.co/api/people/18/') {
+        if (charac.search('/18/') > 0) {
           counter++;
         }
       }
