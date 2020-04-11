8c7722a38e3c2a31e054c623e59e1534410576a0
Carlos Molano <926@holbertonschool.com>
2020-04-10 19:27:31 -0500

    [Update]: rewriting more pythonic way

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    s_list = list(my_string)
-    i = 0
-    while i < len(s_list):
-        if s_list[i] == 'c' or s_list[i] == 'C':
-            s_list.pop(i)
-        else:
-            i = i + 1
-    new_str = "".join(s_list)
-    return(new_str)
+    my_string_list = list(my_string)
+    new_string = [l for l in my_string_list if l != 'c' and l != 'C']
+    return "".join(new_string)
